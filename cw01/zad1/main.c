402d285c2deba212adbe5964d75f9bbf01d5fabb
PKopel <pawel.kopel2@gmail.com>
2020-10-25 15:22:50 +0100

    zad1 finished

--- cw01/zad1/main.c
+++ cw01/zad1/main.c
@@ -4,15 +4,20 @@ const int READER_TURNS = 10;
 const int WRITER_TURNS = 10;
 const int READERS_COUNT = 5;
 
+const char *READER_SEM = "reader_sem";
+const char *WRITER_SEM = "writer_sem";
+const char *FILE_NAME = "file.txt";
+
 sem_t *writer_sem, *reader_sem;
 int readers_count = 0;
 
 // Writer thread function
 int Writer(void *data)
 {
-    int i;
 
-    for (i = 0; i < WRITER_TURNS; i++)
+    FILE *file = fopen(FILE_NAME, "w");
+
+    for (int i = 0; i < WRITER_TURNS; i++)
     {
         if (sem_wait(writer_sem) != 0)
             error("Error occured during locking the writer semaphore.\n");
@@ -22,8 +27,10 @@ int Writer(void *data)
             // Write
             printf("(W) Writer started writing...");
             fflush(stdout);
+            fprintf(file, "%02d\t", i);
+            fflush(file);
             usleep(get_random_time(800));
-            printf("finished\n");
+            printf("(W) finished\n");
 
             // Release ownership of the mutex object.
             if (sem_post(writer_sem) != 0)
@@ -33,6 +40,8 @@ int Writer(void *data)
         }
     }
 
+    fclose(file);
+
     return 0;
 }
 
@@ -41,6 +50,10 @@ int Reader(void *data)
 {
     int i;
     int threadId = *(int *)data;
+
+    FILE *file = fopen(FILE_NAME, "r");
+    char *read_buffer =
+        (char *)calloc(10, sizeof(char));
 
     for (i = 0; i < READER_TURNS; i++)
     {
@@ -58,9 +71,10 @@ int Reader(void *data)
             // Read
             printf("(R) Reader %d started reading...", threadId);
             fflush(stdout);
-            // Read, read, read
+            fread(read_buffer, sizeof(char), 10, file);
+            printf("(R) Reader %d read \"%s\"", threadId, read_buffer);
             usleep(get_random_time(200));
-            printf("finished\n");
+            printf("(R) Reader %d finished\n", threadId);
 
             if (sem_wait(reader_sem) != 0)
                 error("Error occured during locking the reader semaphore.\n");
@@ -78,7 +92,9 @@ int Reader(void *data)
         }
     }
 
-    free(data);
+    free((void *)data);
+    free((void *)read_buffer);
+    fclose(file);
 
     return 0;
 }
@@ -92,10 +108,10 @@ int main(int argc, char *argv[])
 
     int i, rc;
 
-    if ((writer_sem = sem_open("writer", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
+    if ((writer_sem = sem_open(WRITER_SEM, O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
         error("create writer sem");
 
-    if ((reader_sem = sem_open("reader", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
+    if ((reader_sem = sem_open(READER_SEM, O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
         error("create writer sem");
 
     // Create the Writer thread
@@ -140,10 +156,10 @@ int main(int argc, char *argv[])
     // Wait for the Writer
     pthread_join(writerThread, NULL);
 
-    if (sem_unlink(writer_sem) < 0)
+    if (sem_unlink(WRITER_SEM) < 0)
         error("writer sem unlink");
 
-    if (sem_unlink(reader_sem) < 0)
+    if (sem_unlink(READER_SEM) < 0)
         error("reader sem unlink");
     return (0);
 }
