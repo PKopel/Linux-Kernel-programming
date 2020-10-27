92374f3e736a9aa0d76157f42d32aa244becc106
PKopel <pawel.kopel2@gmail.com>
2020-10-27 18:31:20 +0100

    zad6: finished

--- cw01/zad6/main.c
+++ cw01/zad6/main.c
@@ -5,22 +5,26 @@ const int WRITER_TURNS = 10;
 const int READERS_COUNT = 5;
 const int WRITERS_COUNT = 5;
 
-const int BUFFER_SIZE = 1000;
+const int BUFFER_SIZE = 100;
 
 const char *READER_SEM = "reader_sem";
 const char *WRITER_SEM = "writer_sem";
 const char *FILE_NAME = "file.txt";
 
 sem_t *writer_sem, *reader_sem;
-pthread_mutex_t buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t buffer_empty = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t buffer_empty_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t buffer_empty_cond = PTHREAD_COND_INITIALIZER;
+int buffer_empty = 0;
+pthread_mutex_t buffer_full_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t buffer_full_cond = PTHREAD_COND_INITIALIZER;
+int buffer_full = 1;
 int readers_count = 0;
 
 char *buffer;
 int buffer_index = 0;
 
-// buffer writer function
-int buffer_writer(void *data)
+// writer function
+int writer(void *data)
 {
     int threadId = *(int *)data;
 
@@ -30,20 +34,32 @@ int buffer_writer(void *data)
     {
         sprintf(message, "W %03d %03d\n", threadId, i);
 
-        if (pthread_mutex_lock(&buffer_mutex) != 0)
+        if (pthread_mutex_lock(&buffer_empty_mutex) != 0)
             error("Error occured during locking the buffer mutex.\n");
-        while (BUFFER_SIZE <= buffer_index)
-            pthread_cond_wait(&buffer_empty, &buffer_mutex);
+        while (buffer_empty)
+            pthread_cond_wait(&buffer_empty_cond, &buffer_empty_mutex);
+
+        if (sem_wait(writer_sem) != 0)
+            error("Error occured during locking the writer semaphore.\n");
 
         printf("(W) writer %d started writing...", threadId);
 
         strcpy(&buffer[buffer_index], message);
 
         buffer_index += 10;
+        if (buffer_index >= BUFFER_SIZE)
+        {
+            buffer_full = 0;
+            buffer_empty = 1;
+            pthread_cond_broadcast(&buffer_full_cond);
+        }
         usleep(get_random_time(400));
         printf("(W) writer %d finished\n", threadId);
 
-        if (pthread_mutex_unlock(&buffer_mutex) != 0)
+        if (sem_post(writer_sem) != 0)
+            error("Error occured during unlocking the writer semaphore.\n");
+
+        if (pthread_mutex_unlock(&buffer_empty_mutex) != 0)
             error("Error occured during unlocking the buffer mutex.\n");
 
         usleep(get_random_time(500));
@@ -54,57 +70,23 @@ int buffer_writer(void *data)
     return 0;
 }
 
-// writer thread function
-int main_writer(void *data)
-{
-
-    FILE *file = fopen(FILE_NAME, "w");
-
-    for (int i = 0; i < WRITER_TURNS; i++)
-    {
-        if (sem_wait(writer_sem) != 0)
-            error("Error occured during locking the writer semaphore.\n");
-
-        // write
-        printf("(W) writer started writing...\n");
-        printf("Buffer: %s\n", buffer);
-        fprintf(file, "%s", buffer);
-        fflush(file);
-
-        if (pthread_mutex_lock(&buffer_mutex) != 0)
-            error("Error occured during locking the buffer mutex.\n");
-        buffer_index = 0;
-        pthread_cond_broadcast(&buffer_empty);
-
-        if (pthread_mutex_unlock(&buffer_mutex) != 0)
-            error("Error occured during unlocking the buffer mutex.\n");
-
-        usleep(get_random_time(800));
-        printf("(W) finished\n");
-
-        // Release ownership of the semapthore object.
-        if (sem_post(writer_sem) != 0)
-            error("Error occured during unlocking the writer semaphore.\n");
-        // _think, think, think, think
-        usleep(get_random_time(1000));
-    }
-
-    fclose(file);
-
-    return 0;
-}
-
 // reader thread function
 int reader(void *data)
 {
     int threadId = *(int *)data;
 
-    FILE *file = fopen(FILE_NAME, "r");
     char *read_buffer =
         (char *)calloc(10, sizeof(char));
 
     for (int i = 0; i < READER_TURNS; i++)
     {
+
+        if (pthread_mutex_lock(&buffer_full_mutex) != 0)
+            error("Error occured during locking the buffer mutex.\n");
+
+        while (buffer_full)
+            pthread_cond_wait(&buffer_full_cond, &buffer_full_mutex);
+
         if (sem_wait(reader_sem) != 0)
             error("Error occured during locking the reader semaphore.\n");
         readers_count++;
@@ -117,10 +99,17 @@ int reader(void *data)
         // Read
         printf("(R) reader %d started reading...", threadId);
         fflush(stdout);
-        fread(read_buffer, sizeof(char), 10, file);
+        buffer_index -= 10;
+        if (buffer_index == 0)
+        {
+            buffer_full = 1;
+            buffer_empty = 0;
+            pthread_cond_broadcast(&buffer_empty_cond);
+        }
+        strncpy(read_buffer, &buffer[buffer_index], 10);
         printf("(R) reader %d read \"%s\"", threadId, read_buffer);
         usleep(get_random_time(200));
-        printf("(R) reader %d finished\n", threadId);
+        printf("(R) reader %d finished, buffer index %d\n", threadId, buffer_index);
 
         if (sem_wait(reader_sem) != 0)
             error("Error occured during locking the reader semaphore.\n");
@@ -132,13 +121,14 @@ int reader(void *data)
         if (sem_post(reader_sem) != 0)
             error("Error occured during unlocking the reader semaphore.\n");
 
+        if (pthread_mutex_unlock(&buffer_full_mutex) != 0)
+            error("Error occured during unlocking the buffer mutex.\n");
+
         usleep(get_random_time(800));
     }
 
     free((void *)data);
     free((void *)read_buffer);
-    fclose(file);
-
     return 0;
 }
 
@@ -149,27 +139,16 @@ int main(int argc, char *argv[])
     buffer =
         (char *)calloc(BUFFER_SIZE, sizeof(char));
 
-    pthread_t main_writer_thread;
-    pthread_t buffer_writer_threads[WRITERS_COUNT];
+    pthread_t writer_threads[WRITERS_COUNT];
     pthread_t reader_threads[READERS_COUNT];
 
     int i, rc;
-
+    sem_unlink(WRITER_SEM);
     if ((writer_sem = sem_open(WRITER_SEM, O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
         error("create writer sem");
-
+    sem_unlink(READER_SEM);
     if ((reader_sem = sem_open(READER_SEM, O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
         error("create writer sem");
-
-    // Create the writer thread
-    rc = pthread_create(
-        &main_writer_thread, // thread identifier
-        NULL,                // thread attributes
-        (void *)main_writer, // thread function
-        (void *)NULL);       // thread function argument
-
-    if (rc != 0)
-        error("Couldn't create the buffer writer threads");
 
     for (i = 0; i < WRITERS_COUNT; i++)
     {
@@ -178,10 +157,10 @@ int main(int argc, char *argv[])
         int *threadId = malloc(sizeof(int));
         *threadId = i;
         rc = pthread_create(
-            &buffer_writer_threads[i], // thread identifier
-            NULL,                      // thread attributes
-            (void *)buffer_writer,     // thread function
-            (void *)threadId);         // thread function argument
+            &writer_threads[i], // thread identifier
+            NULL,               // thread attributes
+            (void *)writer,     // thread function
+            (void *)threadId);  // thread function argument
 
         if (rc != 0)
             error("Couldn't create the buffer writer threads");
@@ -212,10 +191,7 @@ int main(int argc, char *argv[])
 
     // Wait for the writers
     for (i = 0; i < READERS_COUNT; i++)
-        pthread_join(buffer_writer_threads[i], NULL);
-
-    // Wait for the writer
-    pthread_join(main_writer_thread, NULL);
+        pthread_join(writer_threads[i], NULL);
 
     if (sem_unlink(WRITER_SEM) < 0)
         error("writer sem unlink");
